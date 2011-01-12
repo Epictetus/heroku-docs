a96ca12a7ce765e915f1ff1f437c3689abcedb70
Morten Bagai <morten@heroku.com>
2011-01-11 16:59:58 -0800

    ticketly docs

--- toc.rb
+++ toc.rb
@@ -74,6 +74,7 @@ section 'add-ons', "Add-ons" do
 	topic 'redistogo',          'Redis To Go'
 	topic 'sendgrid',           'Sendgrid SMTP'
 	topic 'ssl',                'SSL'
+	topic 'ticketly'            'Ticketly'
 	topic 'websolr',            'Websolr search'
 	topic 'zerigo',             'Zerigo DNS'
 end
